c12e2aa7a3fe51c409bfd09d433a928b40b2086c
Kamil Kałuża <kamil321.94@gmail.com>
2022-12-03 10:47:58 +0100

    p

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,37 @@
-const AddTransaction = () => {
-    return(
-        <>
-            <h3>Add new transaction</h3>
-            <form >
-                <div className="form-control">
-                    <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text.."/>
-                </div>
-                <div className="form-control">
-                    <label htmlFor="amount">Amount <br/> (negative - expensive - income)</label>
-                    <input type="number"  placeholder="Enter amount.." />
-                </div>
-            </form>
-        </>
-    )
-}
+import { useState } from "react";
 
-export default AddTransaction
+const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text.."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br /> (negative - expensive - income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount.."
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
